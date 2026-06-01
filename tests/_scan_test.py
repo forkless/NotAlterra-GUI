201d2b22ba56d1223a05479a51732f2aea450102
forkless <forkless@users.noreply.github.com>
2026-06-01 02:50:02 +0200

    Gitignore tests/ folder

--- tests/_scan_test.py
+++
@@ -1,9 +0,0 @@
-import struct
-d = open('samples/savegame_0.sav','rb').read()
-i = d.find(b'ElapsedTimeDouble')
-print('ElapsedTimeDouble found at:', i)
-i2 = d.find(b'Elapsed')
-print('Elapsed found at:', i2)
-# Check FName encoding at position i2-4
-print('u32 at i2-4:', struct.unpack_from('<I', d, i2-4)[0])
-print('Expected:', len(b'Elapsed')+1)
